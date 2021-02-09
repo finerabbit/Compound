d67022e5678fe35a4615123a73bb55bd37c6c48f
unknown <luckyrabbit870930@gmail.com>
2021-02-10 07:05:42 +0900

    wallet

--- SupplyingAssets.js
+++ SupplyingAssets.js
@@ -1,2 +1,11 @@
 const Web3 = require('web3');
 const web3 = new Web3('https://ropsten.infura.io/v3/' + process.env.API_KEY);
+require('dotenv').config();
+
+const privateKey = process.env.PRIVATE_KEY;
+
+web3.eth.accounts.wallet.add('0x' + privateKey);
+const myWalletAddress = web3.eth.accounts.wallet[0].address;
+
+console.log(privateKey);
+console.log(myWalletAddress);
